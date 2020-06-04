de6d61c851de1efd0f0215feb8b3056b2905cbb8
Krishna Mohan <krishnamohan@authenticid.co>
2020-06-04 17:08:36 +0530

    first commit

---
+++ Example/Pods/Target Support Files/CFAScan/CFAScan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CFAScan : NSObject
+@end
+@implementation PodsDummy_CFAScan
+@end
---
+++ ObjectClass.m
@@ -0,0 +1,13 @@
+//
+//  ObjectClass.m
+//  DemoApp
+//
+//  Created by AID011 on 04/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import "ObjectClass.h"
+
+@implementation ObjectClass
+
+@end
